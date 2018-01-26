435e02970b7725fc2028c7f6ac0ffeda2c27ae07
john m <35587118+jjmareck@users.noreply.github.com>
2018-01-25 21:30:35 -0600

    Delete main_assignment2.py

--- tests/assignments/main_assignment2.py
+++
@@ -1,4 +0,0 @@
-from assignment2 import faculty_evaluation_result
-
-'''Write code to call the faculty_evaluation_result function with data of your choice'''
-faculty_evaluation_result(1,4,6,5,40,50)
