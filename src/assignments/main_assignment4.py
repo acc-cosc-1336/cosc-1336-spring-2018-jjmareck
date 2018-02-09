f7b1db8b0204db734e68d3f4fd8312f4d35ccfac
john m <35587118+jjmareck@users.noreply.github.com>
2018-02-08 21:28:01 -0600

    Add files via upload
    completed assignment 4

---
+++ src/assignments/main_assignment4.py
@@ -0,0 +1,22 @@
+def main():#void function
+    '''
+    Create a loop that'll run until the user doesn't type the letter 'y'
+    In the loop,
+    Capture one number from the keyboard and validate for a range between 1 and 10.
+    Call the factorial function.
+    Save the result to a variable.
+    Print the variable value.
+
+    Ask the user if they want to continue.
+
+    TO RUN YOUR PROGRAM GO TO IN PYTHON IDLE RUN->RUN MODULE.
+    IN THE PYTHON SHELL TYPE main()
+
+    DON'T ADD A RETURN STATEMENT TO THIS FUNCTION
+    '''
+
+
+
+
+
+#another option to run the program is to call the main() function below and Run->Run Module
