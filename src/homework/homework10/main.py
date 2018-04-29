de3a49b9802aed08147ba3db92e29e35d57fc1da
john m <35587118+jjmareck@users.noreply.github.com>
2018-04-29 15:00:16 -0500

    Add files via upload
    hw10

--- src/homework/homework10/main.py
+++ src/homework/homework10/main.py
@@ -1,1 +1,15 @@
+from src.homework.homework10.player import Player
+from src.homework.homework10.game_log import GameLog
 
+#write import statement for GameLog class
+
+
+#Create a game log instance
+game_log = GameLog()
+#SEnd the game_log instance to Player class as an argument
+Player(game_log).roll_doubles()
+
+
+#call the game log display method below
+
+game_log.display_log()
